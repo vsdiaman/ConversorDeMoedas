93199c731cfd3ad6da68d56185185e3a3d862ffa
vsdiaman <vitor.diamantino8@gmail.com>
2021-12-19 03:07:45 -0300

    Projeto iniciando

--- src/App.js
+++ src/App.js
@@ -1,24 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React from "react";
+import {
+  Button,
+  Form,
+  Col,
+  Spinner,
+  Jumbotron,
+  Alert,
+  Modal,
+  Container,
+  Row,
+} from "react-bootstrap";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faAngleDoubleRight } from "@fortawesome/free-solid-svg-icons";
+import ListarMoedas from "components/List/listar-moedas";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <h1>Conversor de moedas</h1>
+      <Alert variant="danger" show={true}>
+        Erro obtendo dados de conversão, tente novamente .
+      </Alert>
+      <Jumbotron>
+        <Form>
+          <Row>
+            <Col sm="3">
+              <Form.Control placeholder="0" value={1} required />
+            </Col>
+            <Col sm="3">
+              <Form.Control as="select">
+                <ListarMoedas />
+              </Form.Control>
+            </Col>
+            <Col sm="1" className="text-center" style={{ paddingTop: "5px" }}>
+              <FontAwesomeIcon icon={faAngleDoubleRight} />
+            </Col>
+            <Col sm="3">
+              <Form.Control as="select">
+                <ListarMoedas />
+              </Form.Control>
+            </Col>
+            <Col sm="2">
+              <Button variant="success" type="submit">
+                <Spinner animation="border" size="sm" />
+                Converter
+              </Button>
+            </Col>
+          </Row>
+        </Form>
+        <Modal show={false}>
+          <Modal.Header closeButton>
+            <Modal.Title>Conversão</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>Resultado da conversão aqui...</Modal.Body>
+          <Modal.Footer>
+            <Button variant="success">Nova Conversão</Button>
+          </Modal.Footer>
+        </Modal>
+      </Jumbotron>
+    </Container>
   );
 }
 
